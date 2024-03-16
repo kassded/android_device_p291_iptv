158c8fb0ba902f26d9392e94b3aad9406b5b61dc
kassded <124594552+kassded@users.noreply.github.com>
2024-03-17 00:24:45 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,22 @@
 #
 
 LOCAL_PATH := device/fiberhome/p291_iptv
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
+    bootctrl.p291_iptv \
+    bootctrl.p291_iptv.recovery \
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
